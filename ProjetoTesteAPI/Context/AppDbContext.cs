e26156717b03ce858b4f1dd7072c3499edb5ebab
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-13 16:48:45 -0300

    Criação do Context

---
+++ ProjetoTesteAPI/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoTesteAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+    }
+}
